83989f3621e1f323c23002ac726252485badc950
Douglas Keeble <douglaskeeble@gmail.com>
2019-04-22 23:18:28 -0600

    V1.8

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-$("#narcDialog").dialog({
-	autoOpen: false
-});
-$("#burgDialog").dialog({
-	autoOpen: false
-});
-$("#homDialog").dialog({
-	autoOpen: false
-});
-
-$('.narcInfo').click(function() {
-	$('#narcDialog').dialog('open');
-})
-$('.burgInfo').click(function() {
-	$('#burgDialog').dialog('open');
-})
-$('.homInfo').click(function() {
-	$('#homDialog').dialog('open');
-})
-
 $(document).ready(function() {
+
+	$('.narcInfo').click(function() {
+		$('#narcDialog').dialog('open');
+	})
+	$('.burgInfo').click(function() {
+		$('#burgDialog').dialog('open');
+	})
+	$('.homInfo').click(function() {
+		$('#homDialog').dialog('open');
+	})
 
 	var coords = [34.0522, -118.2437, 10];
 	var map = L.map('map', {
@@ -28,12 +18,17 @@ $(document).ready(function() {
 		zoomControl: false
 	});
 
+	L.svg({
+		clickable: true
+	}).addTo(map);
+
 	var basemap = L.tileLayer(
 		'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 			subdomains: 'abcd',
 			maxZoom: 19
 		}).addTo(map);
+
 
 	var heatAll = [];
 	var heatNarc = [];
@@ -43,6 +38,8 @@ $(document).ready(function() {
 	var heatmapBurg;
 	var heatmapHom;
 	var heatmapNarc;
+	var props = [];
+	var expressed;
 
 	zoomButtons();
 
@@ -99,7 +96,202 @@ $(document).ready(function() {
 		});
 	});
 
+	createTopo();
 	jqueryInit();
+
+	function createTopo() {
+		var csv;
+		d3.csv("data/dem16.csv").then(function(data) {
+			csv = data;
+		});
+
+		d3.json('data/dem16topo.json').then(function(data) {
+			var svg = d3.select("#map").select("svg")
+				.attr("pointer-events", "auto");
+
+			var g = svg.select("g");
+
+			d3.selectAll("g")
+				.classed("dem16Layer", true);
+
+			var topo = topojson.feature(data, data.objects.dem16).features;
+
+
+			var joined = joinData(topo, csv);
+			var colorScale = makeColorScale(csv);
+
+			function projectPoint(x, y) {
+				var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+				this.stream.point(point.x, point.y);
+			};
+
+			var transform = d3.geoTransform({
+				point: projectPoint
+			});
+
+			var path = d3.geoPath().projection(transform);
+
+
+			var feature = g.selectAll("path")
+				.data(joined)
+				.enter()
+				.append("path")
+				.attr("class", function(d) {
+					return "tract ct" + d.properties.CT10;
+				})
+				.attr("d", path)
+				.style("fill", function(d) {
+					return choropleth(d.properties, colorScale);
+				}).on("mouseover", function(d) {
+					highlight(d.properties);
+				})
+				.on("mouseout", function(d) {
+					dehighlight(d.properties);
+				});
+			var desc = feature.append("desc")
+				.text('{"stroke-width": "0"}');
+
+			map.on("moveend", update);
+
+			update();
+
+			function update() {
+				feature.attr("d", path);
+			}
+
+		})
+
+		//function to highlight enumeration units and bars
+		function highlight(properties) {
+			var selected = d3.selectAll(".ct" + properties.CT10)
+				.style("stroke", "red")
+				.style("stroke-width", "2");
+			$('#infoBar').html(props[props.indexOf(expressed)] + ' ' + properties[
+				expressed]);
+			$('#infoBar').removeClass('no-show');
+		};
+
+		function dehighlight(properties) {
+			var selected = d3.selectAll(".ct" + properties.CT10)
+				.style("stroke-width", function() {
+					return getStyle(this, "stroke-width")
+				});
+			$('#infoBar').empty();
+			$('#infoBar').addClass('no-show');
+
+			function getStyle(element, styleName) {
+				var styleText = d3.select(element)
+					.select("desc")
+					.text();
+
+				var styleObject = JSON.parse(styleText);
+
+				return styleObject[styleName];
+			};
+		};
+
+
+		function joinData(dem16, csvData, ) {
+			//join geoid of data and tracts
+			for (prop in csvData[0]) {
+				if (!(prop == 'CT10')) {
+					props.push(prop)
+				}
+			}
+			props.forEach(function(val) {
+				$('#demSelect').append('<option value="' + val + '">' + val +
+					'</option>');
+			});
+
+			$('#demSelect').change(function() {
+				changeAttribute(this.value, csvData)
+			})
+
+			expressed = props[0];
+
+			for (i = 0; i < csvData.length; i++) {
+				var currTract = csvData[i];
+				var csvKey = currTract.CT10;
+
+				for (j = 0; j < dem16.length; j++) {
+					var jsonProps = dem16[j].properties;
+					var jsonKey = jsonProps.CT10;
+
+					if (jsonKey == csvKey) {
+						props.forEach(function(val) {
+							jsonProps[val] = currTract[val]
+						})
+					}
+				}
+			}
+			return dem16
+		}
+	}
+
+	function makeColorScale(data) {
+		var colorClasses = [
+			"#d0d1e6",
+			"#a6bddb",
+			"#67a9cf",
+			"#1c9099",
+			"#016c59",
+
+		];
+
+		//create color scale generator
+		var colorScale = d3.scaleThreshold()
+			.range(colorClasses);
+
+		//build array of all values of the expressed attribute
+		var domainArray = [];
+		for (var i = 0; i < data.length; i++) {
+			var val = parseFloat(data[i][expressed]);
+			domainArray.push(val);
+		};
+
+		//cluster data using ckmeans clustering algorithm to create natural breaks
+		var clusters = ss.ckmeans(domainArray, 5);
+		//reset domain array to cluster minimums
+		domainArray = clusters.map(function(d) {
+			return d3.min(d);
+		});
+		//remove first value from domain array to create class breakpoints
+		domainArray.shift();
+
+		//assign array of last 4 cluster minimums as domain
+		colorScale.domain(domainArray);
+
+		return colorScale;
+	};
+
+	//function to test for data value and return color
+	function choropleth(props, colorScale) {
+		//make sure attribute value is a number
+		var val = parseFloat(props[expressed]);
+		//if attribute value exists, assign a color; otherwise assign gray
+		if (typeof val == 'number' && !isNaN(val)) {
+			return colorScale(val);
+		} else {
+			return "#CCC";
+		};
+	};
+
+
+	function changeAttribute(attribute, csvData) {
+		//change the expressed attribute
+		expressed = attribute;
+
+		//recreate the color scale
+		var colorScale = makeColorScale(csvData);
+
+		//recolor enumeration units
+		var tracts = d3.selectAll("path")
+			.transition()
+			.duration(1000)
+			.style("fill", function(d) {
+				return choropleth(d.properties, colorScale)
+			});
+	};
 
 	function zoomButtons() {
 		var zoom = L.control({
@@ -134,13 +326,12 @@ $(document).ready(function() {
 			if ($(this).hasClass('activeYear')) {
 				$(this).toggleClass('activeYear');
 				map.removeLayer(heatmapNarc);
-				map.addLayer(heatmapAll);
-				$('.all16').toggleClass('activeYear');
 			} else {
 				$(this).toggleClass('activeYear');
 				map.eachLayer(function(layer) {
 					if (!(layer._url ==
-							'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png')) {
+							'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png') &&
+						!(layer.options.clickable == true)) {
 						map.removeLayer(layer);
 					}
 				});
@@ -154,13 +345,13 @@ $(document).ready(function() {
 			if ($(this).hasClass('activeYear')) {
 				$(this).toggleClass('activeYear');
 				map.removeLayer(heatmapHom);
-				map.addLayer(heatmapAll);
-				$('.all16').toggleClass('activeYear');
 			} else {
 				$(this).toggleClass('activeYear');
 				map.eachLayer(function(layer) {
+					console.log(layer);
 					if (!(layer._url ==
-							'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png')) {
+							'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png') &&
+						!(layer.options.clickable == true)) {
 						map.removeLayer(layer);
 					}
 				});
@@ -174,14 +365,13 @@ $(document).ready(function() {
 			if ($(this).hasClass('activeYear')) {
 				$(this).toggleClass('activeYear');
 				map.removeLayer(heatmapBurg);
-				map.addLayer(heatmapAll);
-				$('.all16').toggleClass('activeYear');
 			} else {
 				$(this).toggleClass('activeYear');
 				map.eachLayer(function(layer) {
 					if (!(layer._url ==
 							'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
-						)) {
+						) &&
+						!(layer.options.clickable == true)) {
 						map.removeLayer(layer);
 					}
 				});
@@ -199,7 +389,8 @@ $(document).ready(function() {
 				$(this).toggleClass('activeYear');
 				map.eachLayer(function(layer) {
 					if (!(layer._url ==
-							'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png')) {
+							'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png') &&
+						!(layer.options.clickable == true)) {
 						map.removeLayer(layer);
 					}
 				});
@@ -207,6 +398,21 @@ $(document).ready(function() {
 				$('.hom16').removeClass('activeYear');
 				$('.burg16').removeClass('activeYear');
 				$('.narc16').removeClass('activeYear');
+			}
+		});
+		$('.dem16').click(function() {
+			d3.selectAll("g")
+				.classed("no-show", function(d, i) {
+					return !d3.select(this).classed("no-show");
+				});
+			if ($(this).hasClass('activeYear')) {
+				$(this).toggleClass('activeYear');
+			} else {
+				$(this).toggleClass('activeYear');
+				$('.hom16').removeClass('activeYear');
+				$('.burg16').removeClass('activeYear');
+				$('.narc16').removeClass('activeYear');
+				$('.all16').removeClass('activeYear');
 			}
 		});
 	}
